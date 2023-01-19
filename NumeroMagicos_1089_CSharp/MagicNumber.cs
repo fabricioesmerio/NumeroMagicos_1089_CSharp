f96b3a826811fa260c4c7f932fa882a698471c9c
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2023-01-19 01:31:11 -0300

    firt commit

---
+++ NumeroMagicos_1089_CSharp/MagicNumber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroMagicos_1089_CSharp
+{
+    public class MagicNumber
+    {
+
+        public void Calculate(int initial, int finish)
+        {
+            for (int currentNumber = initial; currentNumber < finish; currentNumber++)
+            {
+                var isPalindromo = IsPalindrome(currentNumber);
+                if (!isPalindromo)
+                {
+                    var invertNumber = InvertNumber(currentNumber);
+                    var resultSub = Substract(currentNumber, invertNumber);
+                    var resultSum = Sum(resultSub, InvertNumber(resultSub));
+
+                    Console.WriteLine("Número: " + currentNumber + ". Número Mágico: " + resultSum);
+                }
+            }
+        }
+
+        private int InvertNumber(int number)
+        {
+            string numberStr = number.ToString();
+            var result = String.Empty;
+
+            if (number < 100)
+            {
+                result = numberStr[1].ToString() + numberStr[0].ToString() + "0";
+            }else
+            {
+                result = numberStr[2].ToString() + numberStr[1].ToString() + numberStr[0].ToString();
+            }
+
+            return int.Parse(result);
+        }
+
+        private bool IsPalindrome(int currentNumber)
+        {
+            return currentNumber == InvertNumber(currentNumber);
+        }
+
+        private int Sum(int currentNumber, int invertedNumber)
+        {
+            return currentNumber + invertedNumber;
+        }
+
+        private int Substract(int currentNumber, int invertedNumber)
+        {
+            if (currentNumber > invertedNumber)
+                return currentNumber - invertedNumber;
+
+            return invertedNumber - currentNumber;
+        }
+
+    }
+}
---
+++ NumeroMagicos_1089_CSharp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using NumeroMagicos_1089_CSharp;
+
+const int INITIAL_NUMBER = 12;
+const int FINISH_NUMBER = 999;
+
+var magicNumber = new MagicNumber();
+magicNumber.Calculate(INITIAL_NUMBER, FINISH_NUMBER);
